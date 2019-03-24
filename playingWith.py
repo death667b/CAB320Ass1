2b054c832bd4d917325a447bb135c104df8a2753
death667b <idaniel@spinnys.com>
2019-03-24 12:34:49 +1000

    Added a testing file

---
+++ playingWith.py
@@ -0,0 +1,43 @@
+from sokoban import Warehouse
+
+def taboo_cells(warehouse):
+    '''  
+    Identify the taboo cells of a warehouse. A cell inside a warehouse is 
+    called 'taboo' if whenever a box get pushed on such a cell then the puzzle 
+    becomes unsolvable.  
+    When determining the taboo cells, you must ignore all the existing boxes, 
+    simply consider the walls and the target cells.  
+    Use only the following two rules to determine the taboo cells;
+     Rule 1: if a cell is a corner inside the warehouse and not a target, 
+             then it is a taboo cell.
+     Rule 2: all the cells between two corners inside the warehouse along a 
+             wall are taboo if none of these cells is a target.
+    
+    @param warehouse: a Warehouse object
+
+    @return
+       A string representing the puzzle with only the wall cells marked with 
+       an '#' and the taboo cells marked with an 'X'.  
+       The returned string should NOT have marks for the worker, the targets,
+       and the boxes.  
+    '''
+    print('SSTTOOPP')
+    
+
+    return 'fffaaarrrkkk'
+
+def test_taboo_cells():
+    wh = Warehouse()
+    wh.load_warehouse("./warehouses/warehouse_01.txt")
+    expected_answer = '####  \n#X #  \n#  ###\n#   X#\n#   X#\n#XX###\n####  '
+    answer = taboo_cells(wh)
+    fcn = test_taboo_cells    
+    print('<<  Testing {} >>'.format(fcn.__name__))
+    if answer==expected_answer:
+        print(fcn.__name__, ' passed!  :-)\n')
+    else:
+        print(fcn.__name__, ' failed!  :-(\n')
+        print('Expected ');print(expected_answer)
+        print('But, received ');print(answer)
+        
+test_taboo_cells()
